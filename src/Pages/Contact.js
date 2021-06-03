c7d19aa474d09fc2b97b45996c612f26874e695e
Arichson <arichsonh@gmail.com>
2021-06-02 17:38:41 -0700

    created a small contact me form

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -3,6 +3,20 @@ function Other(props) {
     <div className="App">
       <h1>{props.page}</h1>
       <p>arichsonh@gmail.com</p>
+      <form>
+        <div>
+          Name:  {" "} 
+          <input type="text" />
+        </div>
+        <div>
+          Email: {" "} 
+          <input type="text" />
+        </div>
+        <div>
+          Message:  {" "} 
+          <textarea/>
+        </div>
+      </form>
     </div>
   );
 }
